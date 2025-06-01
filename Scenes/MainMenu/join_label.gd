750c30e9b39b92fb12274346fd7fe4ee58c434b9
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-01 15:59:49 +1000

    press any button to join label is always on the right of the screen now

---
+++ Scenes/MainMenu/join_label.gd
@@ -0,0 +1,12 @@
+extends ColorRect
+
+func _enter_tree() -> void:
+	var parent := get_parent()
+	if parent:
+		parent.move_child(self, -1)
+	parent.child_order_changed.connect(_ensure_last_child)
+
+func _ensure_last_child() -> void:
+	var parent := get_parent()
+	if parent and parent.get_child(parent.get_child_count() - 1) != self:
+		parent.move_child(self, parent.get_child_count() - 1)
