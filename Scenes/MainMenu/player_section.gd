85689fcda1902b488de5525aa27f669ae7b114f2
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-01 16:08:58 +1000

    adding some funky physics to the main menu

--- Scenes/MainMenu/player_section.gd
+++ Scenes/MainMenu/player_section.gd
@@ -5,6 +5,11 @@ var controller_id: int = -1
 var player_id: int = -1
 var loaded: bool = false
 
+var spring_velocity: float = 0.0
+
+@export var spring_strength: float = 0.9
+@export var spring_dampening: float = 0.2
+
 @export var player_label: Label
 
 func _process(_delta: float) -> void:
@@ -20,3 +25,11 @@ func _process(_delta: float) -> void:
 		print("start game")
 	elif Input.is_joy_button_pressed(controller_id, JOY_BUTTON_B):
 		PlayerManager.leave_player(controller_id)
+
+func _physics_process(delta: float) -> void:
+	spring_velocity += (1 - size_flags_stretch_ratio) * spring_strength * delta
+	spring_velocity -= spring_velocity * spring_dampening * delta
+	size_flags_stretch_ratio += spring_velocity * delta
+
+	print("Spring velocity: ", spring_velocity)
+	print("Size flags stretch ratio: ", size_flags_stretch_ratio)
