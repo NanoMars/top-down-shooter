9b1e9593a4326622265c0efdeea17de29bd3f78f
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-04 11:55:50 +1000

    spawning players in with new system Define autoload colour palette script

--- Scenes/Game/Player/spawn_point.gd
+++ Scenes/Game/Player/spawn_point.gd
@@ -1,2 +1,4 @@
 extends Marker2D
 
+@export_enum("red", "purple", "yellow", "green") var player_color: int = 0
+
---
+++ scripts/colour_palette.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var red := Color.hex("FC5C65")
+var purple := Color.hex("9179FF")
+var yellow := Color.hex("FFB601")
+var green := Color.hex("38D98B")
