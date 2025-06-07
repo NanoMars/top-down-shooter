19fb515484253a0071368e7c3263f47e5f48fcfc
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-07 14:25:34 +1000

    main menu now autoloads players if there are already players in the game when the scene loads

--- MainMenu/player_menu.gd
+++ MainMenu/player_menu.gd
@@ -3,13 +3,21 @@ extends Node
 
 @export var player_section: PackedScene
 
+func _ready() -> void:
+	for controller_id in PlayerManager.joined_players.keys():
+		var player_id: int = PlayerManager.joined_players[controller_id]
+		add_player_section(controller_id, player_id)
 
 func _input(event):
 	if event is InputEventJoypadButton and event.pressed:
 		var controller_id: int = event.device
 		var player_id: int = PlayerManager.join_player(controller_id)
 		if player_id != -1:
-			var section: Control = player_section.instantiate()
-			section.controller_id = controller_id
-			section.player_id = player_id
-			add_child(section)
+			add_player_section(controller_id, player_id)
+		
+
+func add_player_section(controller_id: int, player_id: int) -> void:
+	var section: Control = player_section.instantiate()
+	section.controller_id = controller_id
+	section.player_id = player_id
+	add_child(section)
